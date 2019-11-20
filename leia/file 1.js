81e741ddd713671db00e55f6ac00ab801fb7d5c5
leiakc <57116108+leiakc@users.noreply.github.com>
2019-11-19 20:59:20 -0500

    image

---
+++ leia/file 1.js
@@ -0,0 +1,17 @@
+
+
+let moonImage;
+
+function preload(){
+moonImage = loadImage(img/moon boy.jpg);
+}
+
+function setup(){
+createCanvas(windowWidth,windowHeight);
+image(moonImage,0,0);
+}
+
+function draw(){
+
+
+}
